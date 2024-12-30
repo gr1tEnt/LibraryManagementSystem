36b5c05f6730e2583d780984b7b3217a86d47b13
Bohdan Vylyvanyi <bodyaviliv@gmail.com>
2024-12-30 22:11:44 +0200

    Create: book class

---
+++ main/src/main/java/com/gr1tEnt/librarymanagementsystem/Book.java
@@ -0,0 +1,121 @@
+package com.gr1tEnt.librarymanagementsystem;
+import jakarta.persistence.*;
+import java.util.Set;
+
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String isbn;
+
+    @Column(nullable = false)
+    private String title;
+
+    @ElementCollection
+    @CollectionTable(name = "book_authors", joinColumns = @JoinColumn(name = "book_id"))
+    @Column(name = "author")
+    private Set<String> authors;
+
+    @Column(nullable = false)
+    private String publisher;
+
+    @Column(nullable = false)
+    private int publicationYear;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private Genre category;
+
+    @Column(nullable = false)
+    private int numberOfCopies;
+
+    @Column(nullable = false)
+    private String shelfLocation;
+
+    @Column(nullable = false)
+    private String status;
+
+    public Book() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Set<String> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<String> authors) {
+        this.authors = authors;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public Genre getCategory() {
+        return category;
+    }
+
+    public void setCategory(Genre category) {
+        this.category = category;
+    }
+
+    public int getNumberOfCopies() {
+        return numberOfCopies;
+    }
+
+    public void setNumberOfCopies(int numberOfCopies) {
+        this.numberOfCopies = numberOfCopies;
+    }
+
+    public String getShelfLocation() {
+        return shelfLocation;
+    }
+
+    public void setShelfLocation(String shelfLocation) {
+        this.shelfLocation = shelfLocation;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
