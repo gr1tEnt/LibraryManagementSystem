36027dec7cfb1480ab0b76942322978ff9c0f342
Bohdan Vylyvanyi <bodyaviliv@gmail.com>
2024-12-30 22:21:39 +0200

    Create: transaction class

---
+++ main/src/main/java/com/gr1tEnt/librarymanagementsystem/Transaction.java
@@ -0,0 +1,77 @@
+package com.gr1tEnt.librarymanagementsystem;
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Column(nullable = false)
+    private LocalDate borrowDate;
+
+    @Column
+    private LocalDate returnDate;
+
+    @Column(nullable = false)
+    private boolean isReturned;
+
+    public Transaction() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public LocalDate getBorrowDate() {
+        return borrowDate;
+    }
+
+    public void setBorrowDate(LocalDate borrowDate) {
+        this.borrowDate = borrowDate;
+    }
+
+    public LocalDate getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(LocalDate returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public boolean isReturned() {
+        return isReturned;
+    }
+
+    public void setReturned(boolean returned) {
+        isReturned = returned;
+    }
+}
