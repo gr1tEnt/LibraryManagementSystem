82c4582d2160d89bb2c06f8ecbf0911e3f97efe1
Bohdan Vylyvanyi <bodyaviliv@gmail.com>
2025-01-16 20:03:40 +0200

    Add: method addBook

--- main/src/main/java/com/gr1tEnt/librarymanagementsystem/service/BookService.java
+++ main/src/main/java/com/gr1tEnt/librarymanagementsystem/service/BookService.java
@@ -1,10 +1,18 @@
 package com.gr1tEnt.librarymanagementsystem.service;
 
 import com.gr1tEnt.librarymanagementsystem.model.Book;
+import com.gr1tEnt.librarymanagementsystem.model.Genre;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class BookService {
     private static final List<Book> books = new ArrayList<>();
+
+    public static Book addBook(Long id, String isbn, String title, Set<String> authors, String publisher, int publicationYear, Genre category) {
+        Book book = new Book(id, isbn, title, authors, publisher, publicationYear, category);
+        books.add(book);
+        return book;
+    }
 }
