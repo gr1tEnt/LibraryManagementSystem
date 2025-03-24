05a6b6db32baf36ff9f80d9b4506d81b6a75fa84
Bohdan Vylyvanyi <bodyaviliv@gmail.com>
2025-03-24 12:09:35 +0200

    Add validation for ISBN-13

--- main/src/main/java/com/gr1tEnt/librarymanagementsystem/utils/InputValidator.java
+++ main/src/main/java/com/gr1tEnt/librarymanagementsystem/utils/InputValidator.java
@@ -52,11 +52,29 @@ public class InputValidator {
                 if (lastChar == 'X') {
                     sum += 10 * 10;
                 } else {
-                    sum += (lastChar - '0') * 10;
+                    sum += (lastChar - '0') * 10;  //// Multiply the last digit by 10
                 }
                 return sum % 11 == 0;
             }
+
+        } else if (isbnInput.length() == 13) {
+            // Regex for ISBN-13 (13 digits)
+            String regex13 = "^[0-9]{13}$";
+            if (isbnInput.matches(regex13)) {
+                // Calculate checksum for ISBN-13
+                int sum = 0;
+                for (int i = 0; i < 13; i++) {
+                    int digit = isbnInput.charAt(i);
+                    if (i % 2 == 0) {
+                        sum += digit; // Odd positions multiply by 1
+                    } else {
+                        sum += digit * 3; // Even positions multiply by 3
+                    }
+                }
+                return sum % 10 == 0;
+            }
         }
+        return false;
     }
 
     public boolean isValidInt(String input) {
