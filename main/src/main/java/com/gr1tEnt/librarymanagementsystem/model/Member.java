f6f2d79513772428264861a3913643674199973e
Bohdan Vylyvanyi <bodyaviliv@gmail.com>
2025-04-02 12:45:30 +0300

    Update member/transaction structure

--- main/src/main/java/com/gr1tEnt/librarymanagementsystem/model/Member.java
+++ main/src/main/java/com/gr1tEnt/librarymanagementsystem/model/Member.java
@@ -2,14 +2,32 @@ package com.gr1tEnt.librarymanagementsystem.model;
 
 import java.time.LocalDate;
 import java.util.Set;
+import java.util.UUID;
 
 public class Member {
-
-    private Long memberId;
+    private UUID memberId = UUID.randomUUID();
     private String name;
     private String contactInfo; // Could be phone/email
     private MembershipType membershipType;
     private LocalDate joinDate;
-    private Set<Transaction> currentBorrowings;
-    private Set<Transaction> borrowingHistory;
+    private Set<Transaction> transactions;
+
+    public Member(UUID memberId, String name, String contactInfo, MembershipType membershipType) {
+        this.memberId = memberId;
+        this.name = name;
+        this.contactInfo = contactInfo;
+        this.membershipType = membershipType;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "memberId=" + memberId +
+                ", name='" + name + '\'' +
+                ", contactInfo='" + contactInfo + '\'' +
+                ", membershipType=" + membershipType +
+                ", joinDate=" + joinDate +
+                ", transactions=" + transactions +
+                '}';
+    }
 }
