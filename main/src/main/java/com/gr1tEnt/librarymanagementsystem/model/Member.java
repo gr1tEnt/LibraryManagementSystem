2528b65a7f590900c18d6b8da40439ac02eb3713
Bohdan Vylyvanyi <bodyaviliv@gmail.com>
2025-04-21 19:34:15 +0300

    Prepare Member class for JPA & switch to lombok

--- main/src/main/java/com/gr1tEnt/librarymanagementsystem/model/Member.java
+++ main/src/main/java/com/gr1tEnt/librarymanagementsystem/model/Member.java
@@ -1,57 +1,45 @@
 package com.gr1tEnt.librarymanagementsystem.model;
 
-import java.sql.Date;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 import java.time.LocalDate;
-import java.util.Set;
+import java.util.List;
 import java.util.UUID;
 
+@Entity
+@Table(name = "members")
+@NoArgsConstructor // Constructor for JPA
+@Getter
+@Setter
+@ToString
 public class Member {
+    @Id
+    @Column(name = "member_id")
     private UUID memberId = UUID.randomUUID();
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "contact_info")
     private String contactInfo; // Could be phone/email
+
+    @Column(name = "membership_type")
+    @Enumerated(EnumType.STRING)
     private MembershipType membershipType = MembershipType.GUEST;
+
+    @Column(name = "join_date")
     private LocalDate joinDate = LocalDate.now();
-    private Set<Transaction> transactions;
+
+    @OneToMany(mappedBy = "member_id") // member_id from transactions
+    private List<Transaction> transactions;
 
     // Constructor for creating a mew member
     public Member(String name, String contactInfo) {
         this.name = name;
         this.contactInfo = contactInfo;
-    }
-
-    public UUID getMemberId() {
-        return memberId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getContactInfo() {
-        return contactInfo;
-    }
-
-    public MembershipType getMembershipType() {
-        return membershipType;
-    }
-
-    public LocalDate getJoinDate() {
-        return joinDate;
-    }
-
-    public Set<Transaction> getTransactions() {
-        return transactions;
-    }
-
-    @Override
-    public String toString() {
-        return "Member{" +
-                "memberId=" + memberId +
-                ", name='" + name + '\'' +
-                ", contactInfo='" + contactInfo + '\'' +
-                ", membershipType=" + membershipType +
-                ", joinDate=" + joinDate +
-                ", transactions=" + transactions +
-                '}';
     }
 }
