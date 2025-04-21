b280712e07604396ad103f7dd38018ebe209f1a3
Bohdan Vylyvanyi <bodyaviliv@gmail.com>
2025-04-21 18:59:53 +0300

    Prepare Transaction class for JPA

---
+++ main/src/main/java/com/gr1tEnt/librarymanagementsystem/model/OperationType.java
@@ -0,0 +1,6 @@
+package com.gr1tEnt.librarymanagementsystem.model;
+
+public enum OperationType {
+    BORROW,
+    RETURN
+}
--- main/src/main/java/com/gr1tEnt/librarymanagementsystem/model/Transaction.java
+++ main/src/main/java/com/gr1tEnt/librarymanagementsystem/model/Transaction.java
@@ -1,12 +1,36 @@
 package com.gr1tEnt.librarymanagementsystem.model;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 import java.time.LocalDate;
 import java.util.UUID;
 
+@Entity
+@Table(name = "transactions")
+@NoArgsConstructor // Constructor for JPA
+@Getter
+@Setter
+@ToString
 public class Transaction {
+    @Id
+    @Column(name = "transaction_id")
     private UUID transactionId = UUID.randomUUID();
-    private UUID memberId;
-    private UUID bookId;
-    private LocalDate borrowDate;
-    private LocalDate returnDate;
-    private boolean isReturned;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id")
+    private Book book;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "operation_type")
+    private OperationType operation;
+
+    @Column(name = "operation_date")
+    private LocalDate date;
 }
