d7665969ab40517f0c6ca501d94ee7014df5e818
Bohdan Vylyvanyi <bodyaviliv@gmail.com>
2025-04-24 21:22:24 +0300

    Create BookRepository

---
+++ main/src/main/java/com/gr1tEnt/librarymanagementsystem/service/BookRepository.java
@@ -0,0 +1,11 @@
+package com.gr1tEnt.librarymanagementsystem.service;
+
+import com.gr1tEnt.librarymanagementsystem.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, UUID> {
+}
